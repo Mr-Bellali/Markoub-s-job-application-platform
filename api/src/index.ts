7b5f8daff64ceb1dfe30ecbbf672ca14cd2df58e
Yassine Bellali <bellali.yassine52@gmail.com>
2025-12-16 22:31:13 +0100

    Setup the first vision of the project

---
+++ api/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express"
+// import routes from "./routes";
+import { config } from 'dotenv';
+config();
+import cors from 'cors';
+
+const app = express()
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/v1", routes)
+
+app.listen(PORT, () => {
+    console.log(`Access API on http://localhost:${PORT}/api/v0`)
+})
+
+export default app
