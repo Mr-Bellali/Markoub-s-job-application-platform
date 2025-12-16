7b8e7a43037c61ae48d5369da5dc0560b4c4c091
Yassine Bellali <bellali.yassine52@gmail.com>
2025-12-16 23:58:53 +0100

    api: testing drizzle and finished setuping the api container

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,23 @@
-import express, {type Express} from "express";
-
-// import routes from "./routes";
-import { config } from 'dotenv';
-config();
+import express from 'express';
 import cors from 'cors';
+import { db } from './config/index';
 
-const app: Express = express()
+
+const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/v1", routes)
+app.get('/health', async (_req, res) => {
+  try {
+    await db.execute('select 1');
+    res.json({ status: "ok" });
+  } catch (err) {
+    res.status(500).json({ status: 'db not ready' });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Access API on http://localhost:${PORT}/api/v0`)
-})
-
-export default app
+  console.log(`API running on http://localhost:${PORT}`);
+});
