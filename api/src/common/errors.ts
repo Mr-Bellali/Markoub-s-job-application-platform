f26face0d30b079e27a30f0a66e51d87675c3244
Yassine Bellali <bellali.yassine52@gmail.com>
2025-12-17 21:55:51 +0100

    add error codes

---
+++ api/src/common/errors.ts
@@ -0,0 +1,9 @@
+export const ErrorCodes = {
+    // Accounts
+    InvalidJWT: 'invalid_jwt',
+
+    // Generic
+    Unauthorized: 'unauthorized',
+    BadRequest: 'bad_request',
+    InternalServerError: 'internal_server_error',
+  } as const 
