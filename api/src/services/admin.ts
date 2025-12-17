e0684de446a9ef26af51b659cdc2d483970b7ec0
Yassine Bellali <bellali.yassine52@gmail.com>
2025-12-17 22:02:10 +0100

    implemented a adminServices class for separating the DB logic and  validation part

---
+++ api/src/services/admin.ts
@@ -0,0 +1,70 @@
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { admins } from "../db/schema/admins";
+import * as schema from '../db/schema/admins';
+import { adminResponse, createAdminInput } from "../types/admin.validator"
+
+export default class AdminServices {
+    // Class attributes
+    private db: NodePgDatabase<typeof schema> & {
+        $client: Pool;
+    }
+
+    // Object constructor
+    constructor(db: NodePgDatabase<typeof schema> & {
+        $client: Pool;
+    }) {
+        this.db = db
+    }
+
+    // Method to retrieve admins from database
+    async getPaginatedAdmins(): Promise<any>{
+        try {
+            const result = await this.db.select().from(admins);
+            return result;
+        } catch (error) {
+            console.error('Error in getPaginatedAdmins: ', error);
+            throw error;
+        }
+    }
+
+    // Count the admins in the database
+    async countAdmins(): Promise<number>{
+        try {
+            const count = await this.db.$count(admins);
+            return count;
+        } catch (error) {
+            console.error("Error in countAdmins: ", error);
+            throw error;
+        }
+    }
+
+    // Create a new admin
+    async createAdmin(data: createAdminInput): Promise<adminResponse>{
+        try {
+            const result = await this.db.insert(admins).values(data).returning({
+                id: admins.id,
+                email: admins.email,
+                role: admins.role,
+                firstName: admins.firstName,
+                lastName: admins.lastName,
+                createdByAdminId: admins.createdByAdminId
+            });
+            
+            if (!result[0]) {
+                throw new Error("Failed to create admin");
+            }
+            
+            return {
+                email: result[0].email!,
+                role: result[0].role!,
+                firstName: result[0].firstName,
+                lastName: result[0].lastName,
+                createdByAdminId: result[0].createdByAdminId
+            };
+        } catch (error) {
+            console.error("Error in createAdmin: ", error);
+            throw error;
+        }
+    }
+}
