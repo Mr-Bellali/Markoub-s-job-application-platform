862cbda192083981e5ee8bf783e412beb92ef0ce
Yassine Bellali <bellali.yassine52@gmail.com>
2025-12-18 23:45:22 +0100

    updated admin schema to have status and createdAt and deletedAt since I'm planning to updated and soft delete admins

--- api/src/db/schema/admins.ts
+++ api/src/db/schema/admins.ts
@@ -2,11 +2,13 @@ import {
   pgTable, 
   integer, 
   varchar,
-  pgEnum
+  pgEnum,
+  timestamp
 } from "drizzle-orm/pg-core"
 
 // Admin roles
 export const roles = pgEnum("roles", ["superadmin", "standard"])
+export const statusEnum = pgEnum("status", ["active", "deleted"])
 
 // Admin schema
 export const admins = pgTable('admins', {
@@ -15,7 +17,10 @@ export const admins = pgTable('admins', {
   lastName: varchar('last_name', { length: 60}).notNull(),
   email: varchar('email', {length: 100}).unique().notNull(),
   role: roles().default("standard"),
+  status: statusEnum().default("active"),
   createdByAdminId: integer(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull().$onUpdate(() => new Date()),
   hashedPassword: varchar('hashed_password')
 });
 
