14e78ce94f79cde1ddb474fa5997764fea27dc8a
Yassine Bellali <bellali.yassine52@gmail.com>
2025-12-21 00:04:15 +0100

    mada the entry point accept bigger jsons

--- api/src/index.ts
+++ api/src/index.ts
@@ -40,7 +40,10 @@ const swaggerOptions = {
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ extended: true, limit: "2mb" }));
+
 
 // Swagger documentation route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
