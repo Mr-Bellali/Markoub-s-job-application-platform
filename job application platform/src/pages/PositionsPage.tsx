f049192e75c71bfabaa79afdf75f791f4ac9bdd9
Yassine Bellali <bellali.yassine52@gmail.com>
2025-12-23 10:29:44 +0100

    implemented reactQuery to fetch positions

--- job application platform/src/pages/PositionsPage.tsx
+++ job application platform/src/pages/PositionsPage.tsx
@@ -1,45 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import DataGrid from '../components/DataGrid'
-import { getPositions } from '../services/positions';
+import DataGrid from "../components/DataGrid"
+import { getPositions } from "../services/positions"
+import { useQuery } from "@tanstack/react-query"
 
 const ApplicationsPage = () => {
-    const [positions, setPositions] = useState<any[]>([]);
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState<string | null>(null)
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["positions", { page: 1, limit: 50 }],
+    queryFn: () => getPositions(1, 50),
+  })
 
-    useEffect(() => {
-        async function load() {
-            setLoading(true)
-            try {
-                const res = await getPositions(1, 50)
-                setPositions(res.data || [])
-            } catch (err: any) {
-                setError(err?.message || 'Failed to load positions')
-            } finally {
-                setLoading(false)
-            }
-        }
-        load()
-    }, [])
+  const positions = data?.data ?? []
 
-    console.log("positions ?", positions);
+  return (
+    <section className="w-full h-screen px-50 pt-20 flex flex-col items-center gap-10">
+      <h1 className="text-4xl font-bold">
+        Open Positions
+      </h1>
 
-    return (
-        <section className='w-full h-screen px-50 pt-20 flex flex-col items-center gap-10'>
-            <h1 className='text-4xl font-bold'>
-                Open Positions
-            </h1>
-            <div className='w-full flex flex-row items-center justify-between text-xl font-semibold'>
-                <h2 className=''>
-                    We have {positions.length} open positions
-                </h2>
-                <div>
-                    dropDown
-                </div>
-            </div>
-            <DataGrid positions={positions} error={error} loading={loading} />
-        </section>
-    )
+      <div className="w-full flex flex-row items-center justify-between text-xl font-semibold">
+        <h2>
+          We have {positions.length} open positions
+        </h2>
+        <div>
+          dropDown
+        </div>
+      </div>
+
+      <DataGrid
+        positions={positions}
+        loading={isLoading}
+        error={isError ? (error as Error).message : null}
+      />
+    </section>
+  )
 }
 
 export default ApplicationsPage
